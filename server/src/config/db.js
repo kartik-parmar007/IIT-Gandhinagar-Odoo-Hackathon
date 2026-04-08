3b7c7b7088833fa6cbf43b6422fbbcd16c8f7c3b
kartik parmar <83116676+kartik-parmar007@users.noreply.github.com>
2026-04-08 20:16:03 +0530

    working model

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+// Fix for querySrv ECONNREFUSED on some networks (e.g. corporate/ISP)
+// This forces Node's internal resolver (c-ares) to use reliable DNS servers
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
